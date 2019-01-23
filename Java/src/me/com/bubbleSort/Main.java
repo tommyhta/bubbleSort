dffe840a78bf77c14e5bdaf51f75e606dce01292
Tommy <tommyhta@gmail.com>
2019-01-23 12:26:11 -0800

    cleaned up code

--- Java/src/me/com/bubbleSort/Main.java
+++ Java/src/me/com/bubbleSort/Main.java
@@ -3,20 +3,36 @@ package me.com.bubbleSort;
 public class Main {
 
     public static void main(String[] args) {
-	    int [] arr = {5,3,6,1,4,2,0};
+	    int [] uAr = {5,3,6,1,4,2,0};
 
-	    //for this implementation, we start with the highest/last index and works toward 0
-	    for(int i = arr.length-1; i>0; i--){
-	        //since the last index would be sorted, this loop will stop before the sorted portion of the array
-	        for(int j = 0; j<i; j++){
-	            Swap(arr, j,(j+1));
+	    cLog(bubbleSort(uAr));
+
+
+    }
+
+
+    //bubble sort
+    public static int[] bubbleSort(int[] arr){
+        //for this implementation, we start with the highest/last index and works toward 0
+        for(int i = arr.length-1; i>0; i--){
+            //since the last index would be sorted, this loop will stop before the sorted portion of the array
+            for(int j = 0; j<i; j++){
+                Swap(arr, j,(j+1));
             }
         }
+        return arr;
+    }
 
-	    for (int i = 0; i<arr.length; i++){
-	        System.out.println(arr[i]);
+
+
+    //traverse through and print the array - for testing
+    public static void cLog(int [] arr){
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
         }
     }
+
+    //external swap method
     public static void Swap(int [] arr, int i, int j){
         if(i == j){
             return;
@@ -28,4 +44,6 @@ public class Main {
             }
         }
     }
+
+
 }
